876bff558091ae9d3590a67eb1e1080e3ef8ce5b
dingeral <a13625816@outlook.com>
2024-07-23 09:21:36 +0800

    fix🐛:

--- jsdelivr/random/autoload.js
+++ jsdelivr/random/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 // const live2d_path =
-//   "https://cdn.jsdelivr.net/gh/dingeral/live2d-widget@latest/";
+//   "https://fastly.jsdelivr.net/gh/dingeral/live2d-widget@latest/";
 
 const live2d_path = "https://cdn.jsdelivr.net/gh/dingeral/live2d-api@latest/";
 
@@ -35,7 +35,6 @@ if (screen.width >= 768) {
       waifuPath: live2d_path + "waifu-tips.json",
 
       // apiPath: "https://live2d.fghrsh.net/api/",
-
       cdnPath: live2d_path,
 
       tools: [
@@ -47,3 +46,23 @@ if (screen.width >= 768) {
     });
   });
 }
+
+console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
--- jsdelivr/sequential/autoload.js
+++ jsdelivr/sequential/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 // const live2d_path =
-//   "https://cdn.jsdelivr.net/gh/dingeral/live2d-widget@latest/";
+//   "https://fastly.jsdelivr.net/gh/dingeral/live2d-widget@latest/";
 
 const live2d_path = "https://cdn.jsdelivr.net/gh/dingeral/live2d-api@latest/";
 
@@ -46,7 +46,26 @@ if (screen.width >= 768) {
         "switch-texture",
         "quit",
       ],
-
     });
   });
 }
+
+console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
